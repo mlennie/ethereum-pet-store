64a65b01124d7909bf2654fabf931661be6b654e
Monty Lennie <montylennie@gmail.com>
2018-01-07 17:00:23 +0000

    add adoption migration file and run migrations

--- truffle.js
+++ truffle.js
@@ -3,8 +3,8 @@ module.exports = {
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
+      host: "0.0.0.0",
+      port: 8545,
       network_id: "*" // Match any network id
     }
   }
